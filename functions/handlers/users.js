0037062514c79d30d2088c8db2111a1693d52fe5
EvocativeAxisPt <61696448+EvocativeAxisPt@users.noreply.github.com>
2021-01-16 23:29:35 +0000

    working on user and notifications

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -117,6 +117,45 @@ exports.addUserDetails = (req,res) => {
   });
 }
 
+//detalhes de utilizador
+exports.getDetalhesUser = (req, res) => {
+  let userData = {};
+  db.doc(`/users/${req.params.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        userData.user = doc.data();
+        return db
+          .collection("screams")
+          .where("userHandle", "==", req.params.handle)
+          .orderBy("createdAt", "desc")
+          .get();
+      } else {
+        return res.status(404).json({ error: "Utilizador não foi encontrado" });
+      }
+    })
+    .then((data) => {
+      userData.screams = [];
+      data.forEach((doc) => {
+        userData.screams.push({
+          body: doc.data().body,
+          createdAt: doc.data().createdAt,
+          userHandle: doc.data().userHandle,
+          userImage: doc.data().userImage,
+          likeCount: doc.data().likeCount,
+          commentCount: doc.data().commentCount,
+          screamId: doc.id,
+        });
+      });
+      return res.json(userData);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+
 //dados do proprio utilizador
 exports.getUserAutenticado = (req,res) => {
   let userData = {};
--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,11 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
+
+const db = require('./util/admin');
 //imports
 const { getAllScreams ,postOneScream,getScream,commentarioEmScream,likeScream,unlikeScream,apagarScream} = require('./handlers/screams'); 
-const { signup, login, uploadImage,addUserDetails,getUserAutenticado} = require('./handlers/users');
+const { signup, login, uploadImage,addUserDetails,getUserAutenticado,getDetalhesUser} = require('./handlers/users');
 
 
 
@@ -26,9 +28,78 @@ app.post('/login',login);
 app.post('/user/image',FBAuth,uploadImage);
 app.post('/user',FBAuth,addUserDetails);
 app.get('/user',FBAuth, getUserAutenticado);
+app.get('/user/:handle',getDetalhesUser);
 
 // express permite exemplo hhtps//blabla.com/API/screams em vez de https//blabla.com/Screams
 exports.api = functions.https.onRequest(app);
+
+
+
+//todo make notifications work
+exports.createNotificationOnLike = functions
+  .firestore.document('likes/{id}')
+  .onCreate((snapshot) => {
+    return db
+      .doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'like',
+            read: false,
+            screamId: doc.id
+          });
+        }
+      })
+      .catch((err) => console.error(err));
+  });
+  
+exports.deleteNotificationOnUnLike = functions
+  .firestore.document('likes/{id}')
+  .onDelete((snapshot) => {
+    return db
+      .doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+  
+exports.createNotificationOnComment = functions
+  .firestore.document('comments/{id}')
+  .onCreate((snapshot) => {
+    return db
+      .doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'comment',
+            read: false,
+            screamId: doc.id
+          });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+
 
 /* exports.getScreams = functions.https.onRequest((req, res)=>{
 admin
