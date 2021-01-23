98eb3b2ee750fbfe55b4d7acd7fa05a18a55b3d7
EvocativeAxisPt <61696448+EvocativeAxisPt@users.noreply.github.com>
2021-01-23 17:10:03 +0000

    cors changes

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,8 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
+const cors = require('cors');
+app.use(cors());
 
 const {db} = require('./util/admin');
 //imports
